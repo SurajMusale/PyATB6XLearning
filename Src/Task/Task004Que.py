26ae92f221974e4f1fa93b3c327f9872029a32fa
hp <surajmusale134@gmail.com>
2025-10-20 17:16:35 +0530

    Commit

---
+++ Src/Task/Task004Que.py
@@ -0,0 +1,21 @@
+# 14 th OCT
+# Write a Python program to calculate the area of a triangle using the formula
+
+#area= 1/2* base*height
+
+# Base=float(input("Enter the base of the circle: "))
+# Height=float(input("Enter the height of the circle: "))
+#
+# Area=0.5*Base*Height
+# print("Area of Circle: ",Area)
+
+#============================================================================================
+
+# Write a program to check if a number entered by the user is greater than 100.
+
+num=int(input("Enter the number: "))
+
+if num>100:
+    print("The no is greater than 100 ")
+else:
+    print("The no is less than 100 ")
