63e0641b4ad84839d02b900b800bc9ab902acef4
hp <surajmusale134@gmail.com>
2025-10-31 09:15:48 +0530

    Commit

---
+++ Src/Ex_07_loops/ForLoop/LabForUsedRealLife.py
@@ -0,0 +1,2 @@
+for test_id in range(1,6):
+    print("running the test case",test_id)
---
+++ Src/Ex_07_loops/WhileLoop/Lab053_While.py
@@ -0,0 +1,9 @@
+#I
+#c
+#u
+
+i=0 #Initialization
+while i<10: #condition
+    print(i)
+
+    i=i+1  #updation
