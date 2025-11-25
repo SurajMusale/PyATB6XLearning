96689c9ffa4e2e116fa990e256bf0b9915118770
hp <surajmusale134@gmail.com>
2025-11-25 22:22:44 +0530

    Commit

---
+++ Src/Task/Task_18Nov_TestFrameworkwithEncapsulationandInheritance.py
@@ -0,0 +1,64 @@
+"""Build a Test Framework with Encapsulation + Inheritance
+
+🎯 Goal:
+
+Create a simple automation structure that uses:
+
+A BaseTest class for setup/teardown (Parent class)
+
+A LoginTest class that inherits BaseTest (Child class)
+
+Encapsulate sensitive data (like credentials) as private variables
+
+
+✅ Requirements:
+
+Create a BaseTest class:
+
+Has a protected variable _driver = "Chrome".
+
+Method setup() prints "Launching browser: Chrome".
+
+Method teardown() prints "Closing browser".
+
+Create a LoginTest class that inherits BaseTest:
+
+Has private variables for username and password.
+
+Method run_test() that prints:
+
+"Running login test with user: <username>".
+
+Use encapsulation: access private vars only through a method (e.g., get_user()).
+
+Create an object of LoginTest and call:
+
+setup()
+
+run_test()
+
+teardown()"""
+
+class BaseTest:
+    _driver = "Chrome"
+
+    def setup(self):
+        print("Launching chrome browser")
+    def teardown(self):
+        print("Closing chrome browser")
+
+class LoginTest(BaseTest):
+    __username = "admin"
+    __password = "admin123"
+
+    def get_user(self):
+        return self.__username
+
+    def run_test(self):
+        user=self.get_user()
+        print("Running login test")
+
+obj=LoginTest()
+obj.setup()
+obj.run_test()
+obj.teardown()
